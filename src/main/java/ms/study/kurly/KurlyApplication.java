83ef56e2663bee87be0f0e0af39cd2a7f085f76a
강민성 <eclipse65@gmail.com>
2023-10-04 20:45:53 +0900

    프로젝트 생성

---
+++ src/main/java/ms/study/kurly/KurlyApplication.java
@@ -0,0 +1,13 @@
+package ms.study.kurly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurlyApplication.class, args);
+	}
+
+}
