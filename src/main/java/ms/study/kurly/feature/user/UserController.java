71aefb6f81f21e6ae9caaba7b28a2f763a8a986d
강민성 <eclipse65@gmail.com>
2023-10-09 17:27:11 +0900

    로그인 API를 구현한다

--- src/main/java/ms/study/kurly/feature/user/UserController.java
+++ src/main/java/ms/study/kurly/feature/user/UserController.java
@@ -2,6 +2,7 @@ package ms.study.kurly.feature.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import ms.study.kurly.feature.user.model.LoginModel;
 import ms.study.kurly.feature.user.model.SignupModel;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,5 +22,11 @@ public class UserController {
     public Boolean isExistEmail(@RequestParam String email) {
 
         return userService.isExistEmail(email);
+    }
+
+    @PostMapping("/login")
+    public Boolean login(@Valid @RequestBody LoginModel dto) {
+
+        return userService.login(dto);
     }
 }
