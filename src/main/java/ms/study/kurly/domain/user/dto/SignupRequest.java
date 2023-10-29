7d3535f97ad98d8f11b7d8b5c88de3eca00f9d85
강민성 <eclipse65@gmail.com>
2023-10-29 14:32:13 +0900

    스웨거 UI를 사용할 수 있도록 세팅한다

--- src/main/java/ms/study/kurly/domain/user/dto/SignupRequest.java
+++ src/main/java/ms/study/kurly/domain/user/dto/SignupRequest.java
@@ -1,12 +1,13 @@
 package ms.study.kurly.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import ms.study.kurly.common.validator.MobileNumber;
 import ms.study.kurly.common.validator.Password;
-import ms.study.kurly.domain.user.User;
 
+@Schema(description = "회원가입 요청")
 @Data
 @Builder
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
@@ -15,17 +16,22 @@ import ms.study.kurly.domain.user.User;
 public class SignupRequest {
 
     @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")
+    @Schema(description = "ID로 사용할 이메일 주소", defaultValue = "aa@bb.cc")
     private String email;
 
     @Password
+    @Schema(description = "비밀번호 (대문자, 소문자, 숫자, 특수문자 각 한자리 이상 포함, 8자 이상)", defaultValue = "Aa12345!")
     private String password;
 
     @NotBlank
+    @Schema(description = "비밀번호 확인", defaultValue = "Aa12345!")
     private String confirmPassword;
 
     @NotBlank
+    @Schema(description = "이름", defaultValue = "홍길동")
     private String name;
 
     @MobileNumber
+    @Schema(description = "휴대폰 번호", defaultValue = "01012345678")
     private String mobileNumber;
 }
--- src/main/java/ms/study/kurly/domain/verification/dto/MobileCodeRequest.java
+++ src/main/java/ms/study/kurly/domain/verification/dto/MobileCodeRequest.java
@@ -1,14 +1,17 @@
 package ms.study.kurly.domain.verification.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import ms.study.kurly.common.validator.MobileNumber;
 
+@Schema(description = "휴대폰 번호 인증 코드 발송 요청")
 @Data
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 @Setter(AccessLevel.PRIVATE)
 public class MobileCodeRequest {
 
+    @Schema(description = "휴대폰 번호", defaultValue = "01012345678")
     @MobileNumber
     private String mobileNumber;
 }
